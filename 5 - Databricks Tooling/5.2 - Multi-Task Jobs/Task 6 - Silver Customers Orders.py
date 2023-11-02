d05c5216971fe7ce383612b38a3f9c88600eb0d8
Shane Short <69159912+sshort1996@users.noreply.github.com>
2023-11-02 16:01:55 +0800

    Create Task 6 - Silver Customers Orders.py

---
+++ 5 - Databricks Tooling/5.2 - Multi-Task Jobs/Task 6 - Silver Customers Orders.py
@@ -0,0 +1,61 @@
+# Databricks notebook source
+# MAGIC %run ../../Includes/Copy-Datasets
+
+# COMMAND ----------
+
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+
+# COMMAND ----------
+
+def batch_upsert(microBatchDF, batchId):
+    window = Window.partitionBy("order_id", "customer_id").orderBy(F.col("_commit_timestamp").desc())
+    
+    (microBatchDF.filter(F.col("_change_type").isin(["insert", "update_postimage"]))
+                 .withColumn("rank", F.rank().over(window))
+                 .filter("rank = 1")
+                 .drop("rank", "_change_type", "_commit_version")
+                 .withColumnRenamed("_commit_timestamp", "processed_timestamp")
+                 .createOrReplaceTempView("ranked_updates"))
+    
+    query = """
+        MERGE INTO customers_orders c
+        USING ranked_updates r
+        ON c.order_id=r.order_id AND c.customer_id=r.customer_id
+            WHEN MATCHED AND c.processed_timestamp < r.processed_timestamp
+              THEN UPDATE SET *
+            WHEN NOT MATCHED
+              THEN INSERT *
+    """
+    
+    microBatchDF.sparkSession.sql(query)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE TABLE IF NOT EXISTS customers_orders
+# MAGIC (order_id STRING, order_timestamp Timestamp, customer_id STRING, quantity BIGINT, total BIGINT, books ARRAY<STRUCT<book_id STRING, quantity BIGINT, subtotal BIGINT>>, email STRING, first_name STRING, last_name STRING, gender STRING, street STRING, city STRING, country STRING, row_time TIMESTAMP, processed_timestamp TIMESTAMP)
+
+# COMMAND ----------
+
+def porcess_customers_orders():
+    orders_df = spark.readStream.table("orders_silver")
+    
+    cdf_customers_df = (spark.readStream
+                             .option("readChangeData", True)
+                             .option("startingVersion", 2)
+                             .table("customers_silver")
+                       )
+
+    query = (orders_df
+                .join(cdf_customers_df, ["customer_id"], "inner")
+                .writeStream
+                    .foreachBatch(batch_upsert)
+                    .option("checkpointLocation", "dbfs:/mnt/demo_pro/checkpoints/customers_orders")
+                    .trigger(availableNow=True)
+                    .start()
+            )
+    
+    query.awaitTermination()
+    
+porcess_customers_orders()
